a7c2ded0f805dc17f2e1f6d5bfa925cb42c53fa1
Javen Wilson <javenb022@gmail.com>
2024-07-24 11:57:24 -0400

    Followed article for setup

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  include RackSessionsFix
+  respond_to :json
+
+  private
+
+  def respond_with(current_user, _opts = {})
+    if resource.persisted?
+      render json:{
+        status: {code: 200, message: 'Signed up successfully.'},
+        data: UserSerializer.new(current_user).serializable_hash[:data][:attributes]
+      }
+    else
+      render json: {
+        status: {message: 'User could not be created successfully. Please try again.'}
+      }, status: :unprocessable_entity
+    end
+  end
+end
